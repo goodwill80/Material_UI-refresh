3fabb8ab78f901cd1ce37019ddaedb667380725f
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-11-07 22:05:40 +0800

    enhancement

--- src/Components/TableMui.tsx
+++ src/Components/TableMui.tsx
@@ -12,28 +12,53 @@ import { TableData } from '../Data';
 
 function TableMui() {
   return (
-    <TableContainer component={Paper} sx={{ maxHeight: '300px' }}>
+    <TableContainer
+      component={Paper}
+      sx={{
+        border: '1px solid rgba(128,128,128,0.4)',
+        width: 'max-content',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 4,
+        borderRadius: 2,
+        maxHeight: 500,
+      }}
+    >
       <Table aria-label="simple table" stickyHeader>
         <TableHead>
-          <TableRow>
+          <TableRow
+            sx={{
+              backgroundColor: 'yellow',
+              borderBottom: '2px solid black',
+              '& th': {
+                fontSize: '1.25rem',
+                color: 'rgba(96, 96, 96)',
+                backgroundColor: 'yellow',
+              },
+            }}
+          >
             <TableCell>Id</TableCell>
             <TableCell>First Name</TableCell>
             <TableCell>Last Name</TableCell>
             <TableCell align="center">Email</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
+        <TableBody
+          sx={{
+            '& tr:nth-of-type(2n+1)': {
+              backgroundColor: 'grey.100',
+            },
+          }}
+        >
           {TableData.map((row, i) => (
             <>
-              <TableRow key={i}>
-                <TableCell rowSpan={3}>{row.id}</TableCell>
-              </TableRow>
               <TableRow
                 key={i}
                 sx={{
                   '&:last-child td, &:last-child th': { border: 0 },
                 }}
               >
+                <TableCell>{row.id}</TableCell>
                 <TableCell>{row.first_name}</TableCell>
                 <TableCell>{row.last_name}</TableCell>
                 <TableCell align="center">{row.email}</TableCell>
